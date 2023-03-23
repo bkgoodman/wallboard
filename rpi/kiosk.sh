8dfffa5bfe09ab0e724257fa7410b12c86b35da9
Brad Goodman <brad@bradgoodman.com>
2023-03-23 10:45:56 -0400

    Added shopbot

--- rpi/kiosk.sh
+++ rpi/kiosk.sh
@@ -11,7 +11,7 @@ echo "{\"addr\":\"$ipv4\"}" > addr.json
 
 #URL='https://1tz7enzkk9.execute-api.us-east-1.amazonaws.com/TEst1/bkg_esp_iot_mqtt?page=pooltemp.html'
 #URL=kiosk.html
-URL=/home/bkg/laserdisp.html
+URL=/home/bkg/wallboard/rpi/shopbot.html
 #URL=XmasKiosk.html
 #chromium-browser $URL --force-media-resolution-width 1980 --force-media-resolution-height 1080 --window-size=1920,1080 --start-fullscreen --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic
 chromium-browser $URL --allow-file-access-from-files --force-media-resolution-width 800 --force-media-resolution-height 450 --window-size=800,450 --start-fullscreen --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic --enable-logging=stderr
